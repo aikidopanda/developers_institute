c6ec9ce4d92429c7ef9d6b4fe86b05bb33303e5c
unknown <moral51288@gmail.com>
2023-03-26 14:14:16 +0300

    daily challenge for python day1 added

---
+++ Week-3/Daily-challenge.py
@@ -0,0 +1,20 @@
+string = input('Enter you message. It must be 10 characters long ')
+if len(string) > 10:
+    print('Your message it too long')
+elif len(string) < 10:
+    print('Your string is too short')
+
+print (string[0])
+print (string[len(string) - 1])
+
+newstring = ''
+for char in range(0, len(string)):
+    newstring = newstring + string[char]
+    print (newstring)
+
+import random
+lst = list(string)
+random.shuffle(lst)
+result = ''.join(lst)
+print (result)
+
