1a9f52e2f2c8f9b1b12d74eb658df6992d81aa28
unknown <moral51288@gmail.com>
2023-04-16 14:03:39 +0300

    update sql

--- Week-5/Day-1/Exercise-XP/Exercise-XP.sql
+++ Week-5/Day-1/Exercise-XP/Exercise-XP.sql
@@ -1,5 +1,5 @@
-create table items (id serial primary key, item varchar, price int)
-create table customers(id serial primary key, fname varchar, lname varchar)
+create table items (id serial primary key, item varchar (10), price int)
+create table customers(id serial primary key, fname varchar(10), lname varchar(10))
 
 select * from items;
 select * from items where price > 80;
