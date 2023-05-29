373dcd74bd6f8f5d7a70f9dffb71f610cb981b60
unknown <moral51288@gmail.com>
2023-05-29 10:34:55 +0300

    xp-exercise

---
+++ Week-9/Day-2/XP-Exercise/script.js
@@ -0,0 +1,48 @@
+//Exercise 1
+function compareToTen(num){
+    let myPromise = new Promise( function(resolve,reject){
+        if (num < 10){
+            resolve('Success')
+        }
+        else{
+            reject('Failure')
+        }
+    }   
+    )
+    return myPromise
+}
+
+compareToTen(8)
+.then(result => console.log(result))
+.catch(error => console.log(error))
+
+//Exercise 2
+let myPromise = new Promise( function(resolve,reject){
+    if (true){
+        resolve(
+            setTimeout(() => {
+                console.log('Success!')
+            },'4000')
+        )
+    }
+    else{ // just a placeholder in this case
+        reject(
+            console.log('OOPS, something went wrong!')
+        )
+    }
+} )
+myPromise.then(result=>console.log(result))
+myPromise.catch(error=>console.log(error))
+
+//Exercise 3
+let switcher = false // for testing purpose
+let newPromise = new Promise( function(resolve,reject){
+    if (switcher == true){
+        resolve(3)
+    }
+    else{
+        reject('Boo!')
+    }
+})
+.then(result=>console.log(result))
+.catch(error=>console.log(error))
