546034ead10f6e3830f3d56fe25ccd26b1519737
unknown <moral51288@gmail.com>
2023-06-06 06:46:55 +0300

    daily-challenge for day-2

---
+++ Week-10/Day-2/Daily-challenge/server.js
@@ -0,0 +1,46 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let app = express()
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+let email
+let msg
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
+app.use(express.static('public'))
+
+app.get('/aboutme/:hobby', (req,res) => {
+    let hobby = req.params.hobby
+    let pattern = (/^[A-Za-z]+$/) // dont know how to do this task without regex, 'typeof' doesnt work because if user
+    // enters numbers they are regarded as a string anyway
+    if (!hobby.match(pattern)){
+        res.status(404).send('Not found')
+    }
+    else{
+        res.send(`<p>My hobby is ${hobby}</p>`)
+    }
+})
+
+app.get('/pic/', (req,res) => {
+    let pictureSrc = '../public/some_cat_image.webp'
+    res.send(`<img src=${pictureSrc} width="300">`) // I dont understand how to send images with this command, always get the path error
+})
+
+app.get('/form', (req,res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/form', urlencodedParser, (req,res) => {
+    console.log(req.body)
+    email = req.body.email
+    msg = req.body.msg
+    res.send(`
+    <p>You sent a message!</p>
+    `)
+})
+
+app.get('/formdata', (req,res) => { 
+    res.send(`
+    <p>${email} has sent you a message: ${msg}</p>
+    `)
+})
